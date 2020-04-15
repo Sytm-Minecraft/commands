18ec1bdc22eb2c33cc8eae16f5fb9dd3223ab52d
Lukas Planz <lukas.planz@web.de>
2020-04-15 17:00:50 +0200

    Added JavaDocs

--- src/main/java/de/md5lukas/commands/BaseCommand.java
+++ src/main/java/de/md5lukas/commands/BaseCommand.java
@@ -1,23 +1,51 @@
 package de.md5lukas.commands;
 
+import com.google.common.base.Preconditions;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Class that is the root of all sub-commands and gets registered as the command executor and tab completer
+ */
 public abstract class BaseCommand extends SubCommand implements CommandExecutor, TabCompleter {
+
+    /**
+     * The name of this command, the same as in the plugin.yml. The name in the plugin.yml has to be lowercase
+     *
+     * @param name The name of this command
+     */
+    public BaseCommand(String name) {
+        super(name);
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return this.onCommand(sender, new LinkedList<>(Arrays.asList(args)));
+        this.onCommand(sender, new LinkedList<>(Arrays.asList(args)));
+        return true;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         return this.onTabComplete(sender, new LinkedList<>(Arrays.asList(args)));
+    }
+
+    /**
+     * Registers this command as the executor and tab completer for the given command name
+     *
+     * @param plugin The plugin to use to register this command
+     */
+    @SuppressWarnings("ConstantConditions")
+    public void register(JavaPlugin plugin) {
+        Preconditions
+                .checkNotNull(plugin.getCommand(getName()), "It seems like the command with the name %s has not been registered in the plugin.yml", getName());
+        plugin.getCommand(getName()).setExecutor(this);
+        plugin.getCommand(getName()).setTabCompleter(this);
     }
 }
--- src/main/java/de/md5lukas/commands/SubCommand.java
+++ src/main/java/de/md5lukas/commands/SubCommand.java
@@ -1,5 +1,6 @@
 package de.md5lukas.commands;
 
+import com.google.common.base.Preconditions;
 import org.bukkit.command.CommandSender;
 
 import java.util.ArrayList;
@@ -8,78 +9,141 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Class for all sub-commands
+ */
 public abstract class SubCommand {
 
-    private List<SubCommand> subCommands;
+    private static final String NOT_IMPLEMENTED = "This method has not been implemented yet. Sub-Command name: %name%";
+    private final String name;
+    private final List<SubCommand> subCommands;
 
-    public SubCommand() {
+    /**
+     * Creates a new sub-command with the provided name.
+     * <br>
+     * The name will be converted to lowercase if he isn't already
+     * <br>
+     * The name of the sub-command is what you would enter after the primary command, for example:
+     * <br>
+     * <ul>
+     *     <li><code>/command name</code></li>
+     *     <li><code>/command sub-command name</code></li>
+     * </ul>
+     *
+     * @param name The name of the command to use
+     * @throws NullPointerException If the name is null
+     */
+    public SubCommand(String name) {
+        this.name = Preconditions.checkNotNull(name, "Th name for the command cannot be null!").toLowerCase();
         subCommands = new ArrayList<>();
     }
 
-    public boolean guard(CommandSender sender, List<String> args) {
+    /**
+     * A guard that is run to check if a CommandSender is allowed to access this command
+     *
+     * @param sender          The sender who issued the command or tab completion
+     * @param args            The arguments that have been entered so far
+     * @param isTabCompletion <code>true</code> if this was caused by an tab completion or not
+     * @return <code>true</code> if the command sender is allowed to access this command
+     */
+    public boolean guard(CommandSender sender, List<String> args, boolean isTabCompletion) {
         return true;
     }
 
+    /**
+     * Adds a new sub command to this command
+     *
+     * @param command The command to add
+     * @return <code>this</code> for chained calls
+     */
     public SubCommand addSubCommand(SubCommand command) {
         subCommands.add(command);
         return this;
     }
 
-    public abstract String getCommand();
+    /**
+     * @return The name of this command
+     */
+    public final String getName() {
+        return name;
+    }
 
+    /**
+     * Optionally you can override this method to also make this sub-command accessible via aliases.
+     * <br><br>
+     * Aliases will not show up in tab-completion
+     *
+     * @return A list of aliases for this commands name
+     */
     public List<String> getAliases() {
         return Collections.emptyList();
     }
 
     //<editor-fold desc="onCommand">
-    public final boolean onCommand(CommandSender sender, LinkedList<String> args) {
-        if (!guard(sender, args)) {
-            return false;
+    void onCommand(CommandSender sender, LinkedList<String> args) {
+        if (!guard(sender, args, false)) {
+            return;
         }
         if (args.isEmpty()) {
-            return run(sender);
+            run(sender);
+            return;
         }
-        String subCommandString = args.getFirst().toLowerCase();
+        String subCommandName = args.getFirst().toLowerCase();
         for (SubCommand subCommand : subCommands) {
-            if (subCommand.getCommand().equals(subCommandString) || subCommand.getAliases().contains(subCommandString)) {
-                return subCommand.onCommand(sender, args);
+            if (subCommand.getName().equals(subCommandName) || subCommand.getAliases().contains(subCommandName)) {
+                subCommand.onCommand(sender, args);
+                return;
             }
         }
-        return run(sender, args);
+        run(sender, args);
     }
 
-    public boolean run(CommandSender sender) {
-        return false;
+    /**
+     * This is the method that is run if no further arguments are provided to this sub command
+     *
+     * @param sender The issuer of the command
+     */
+    public void run(CommandSender sender) {
+        sender.sendMessage(NOT_IMPLEMENTED.replace("%name%", getName()));
     }
 
-    public boolean run(CommandSender sender, List<String> args) {
-        return false;
+    /**
+     * This is the method that is run, if further arguments are provided, but are not matching any sub-command name
+     *
+     * @param sender The issuer of the command
+     * @param args   Any additional arguments
+     */
+    public void run(CommandSender sender, List<String> args) {
+        sender.sendMessage(NOT_IMPLEMENTED.replace("%name%", getName()));
     }
     //</editor-fold>
 
     //<editor-fold desc="onTabComplete">
-    public final List<String> onTabComplete(CommandSender sender, LinkedList<String> args) {
-        if (!guard(sender, args)) {
+    final List<String> onTabComplete(CommandSender sender, LinkedList<String> args) {
+        if (!guard(sender, args, true)) {
             return Collections.emptyList();
         }
-        if (args.isEmpty()) {
-            return tabComplete(sender);
-        }
-        String subCommandString = args.getFirst().toLowerCase();
+        String subCommandName = args.getFirst().toLowerCase();
         for (SubCommand subCommand : subCommands) {
-            if (subCommand.getCommand().equals(subCommandString) || subCommand.getAliases().contains(subCommandString)) {
+            if (subCommand.getName().equals(subCommandName) || subCommand.getAliases().contains(subCommandName)) {
                 return subCommand.onTabComplete(sender, args);
             }
         }
-        List<String> completions = subCommands.stream().map(SubCommand::getCommand).collect(Collectors.toList());
+        // TODO check for subcommand guard
+        List<String> completions = subCommands.stream().map(SubCommand::getName).collect(Collectors.toList());
         completions.addAll(tabComplete(sender, args));
         return completions;
     }
 
-    public List<String> tabComplete(CommandSender sender) {
-        return Collections.emptyList();
-    }
-
+    /**
+     * This is the method that is run if this sub-command is trying to retrieve tab-completions.
+     * <br>
+     * The results of this method are merged with the names of further sub-commands of this sub-command.
+     *
+     * @param sender the issuer of the tab-completion
+     * @param args   The arguments that have been provided so far, can be empty
+     * @return What the issuer should get recommended for tab-completions
+     */
     public List<String> tabComplete(CommandSender sender, List<String> args) {
         return Collections.emptyList();
     }
