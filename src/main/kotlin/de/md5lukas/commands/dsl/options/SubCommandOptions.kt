d1b741c6bd5aa804664325783ec523ae67b54b59
Lukas Planz <lukas.planz@web.de>
2020-09-21 21:01:16 +0200

    Added LGPLv3.0 License headers

--- src/main/kotlin/de/md5lukas/commands/dsl/options/SubCommandOptions.kt
+++ src/main/kotlin/de/md5lukas/commands/dsl/options/SubCommandOptions.kt
@@ -1,3 +1,21 @@
+/*
+ *     commands-kt - Library to define commands in a Kotlin-ish way
+ *     Copyright (C) 2020 Lukas Planz <lukas.planz@web.de>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package de.md5lukas.commands.dsl.options
 
 import org.bukkit.command.CommandSender
